934d38a1bc1ccfcbb7dbc0f61669c5edb2d4735e
a-ptrv <a.ptrv@yandex.ru>
2023-12-08 21:32:11 +0500

    task2

---
+++ lesson06/task02.py
@@ -0,0 +1,6 @@
+first_name = input('Введите Ваше имя: ')
+last_name = input('Введите Вашу фамилию: ')
+age = input('Введите Ваш возраст: ')
+
+inf_list = [first_name, last_name, age]
+print(f'Список данных пользователя: {inf_list}')
