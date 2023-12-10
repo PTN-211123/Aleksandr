7c6f58536858697560f81e566aa622883d90b971
a-ptrv <a.ptrv@yandex.ru>
2023-12-10 14:27:41 +0500

    task 6, 7

---
+++ lesson06/task06.py
@@ -0,0 +1,4 @@
+numbers = [1, 2, 3, 4, 5, 6, 7]
+sqr_list = [i ** 2 for i in numbers]
+print(f'Лист чисел: {numbers}')
+print(f'Лист квадратов: {sqr_list}')
---
+++ lesson06/task07.py
@@ -0,0 +1,7 @@
+my_food_list = ['Плов', 'Манты', 'Шашлык']
+fr_food_list = my_food_list.copy()
+#print(fr_food_list)
+my_food_list.append('Борщ')
+fr_food_list.append('Лагман')
+print(f'Список моих любимых блюд: {my_food_list}')
+print(f'Список любимых блюд друга: {fr_food_list}')
