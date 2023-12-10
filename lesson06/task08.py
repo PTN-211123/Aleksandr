fcfa4f8e6ad5d28b8ccfa2de6d95d710c2bc2608
a-ptrv <a.ptrv@yandex.ru>
2023-12-10 23:42:52 +0500

    task 8.0.2

--- lesson06/task08.py
+++ lesson06/task08.py
@@ -1,10 +1,13 @@
 user_num = int(input('Введите число: '))
-num_list = []
+#num_list = []
 num_sum = 0
-for i in range(user_num):
-    num_list.append(i + 1)
-    num_sum += i + 1
-#num_list = [(i + 1) + for i in range(user_num)]
+# for i in range(user_num):
+#     num_list.append(i + 1)
+#     num_sum += i + 1
+num_list = [(i + 1) for i in range(user_num)]
+
+for i in num_list:
+    num_sum += i
 
 print(f'Список чисел в диапазоне от 1 до {user_num}: {num_list}')
 print(f'Сумма чисел списка равна: {num_sum}')
