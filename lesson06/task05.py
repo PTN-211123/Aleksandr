2681b26d8cb259f257da9fc62509c67261dadc27
a-ptrv <a.ptrv@yandex.ru>
2023-12-10 13:43:25 +0500

    task 5

---
+++ lesson06/task05.py
@@ -0,0 +1,5 @@
+names = input('Введите три любых имени, разделенных пробелом: ')
+names_list = names.split(' ')
+print(f'Первое имя: {names_list[0]}')
+print(f'Второе имя: {names_list[1]}')
+print(f'Третье имя: {names_list[2]}')
