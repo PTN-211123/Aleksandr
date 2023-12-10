e593370ab6f2058864397b54735a455b1af6f78f
a-ptrv <a.ptrv@yandex.ru>
2023-12-10 22:14:31 +0500

    task 8

---
+++ lesson06/task08.py
@@ -0,0 +1,9 @@
+user_num = int(input('Введите число: '))
+num_list = []
+num_sum = 0
+for i in range(user_num):
+    num_list.append(i + 1)
+    num_sum += i + 1
+#num_list = [(i + 1) + for i in range(user_num)]
+print(num_list)
+print(num_sum)
