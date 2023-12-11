74b194600745dbf1c7e266d50fc149d9a51e7d9a
a-ptrv <a.ptrv@yandex.ru>
2023-12-11 13:57:51 +0500

    task 10.0.2

---
+++ lesson06/task11.py
@@ -0,0 +1,10 @@
+print('Количество цифр в числе 75869 равно 5')
+numb = input('Введите свое число: ')
+# cnt = 0
+# for i in numb:
+#     cnt += 1
+#print(f'Количество цифр в числе {numb} равно {cnt}')
+
+num = len(numb)
+print(f'Количество цифр в числе {numb} равно {num}')
+#print(new)
