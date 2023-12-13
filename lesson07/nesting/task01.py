ccda625eac60e8f20bb7ec599de568b7ea7be6e8
a-ptrv <a.ptrv@yandex.ru>
2023-12-13 20:28:52 +0500

    task 1

---
+++ lesson07/nesting/task01.py
@@ -0,0 +1,25 @@
+devices = {
+    'r1': {
+        'location': 'Bukhara',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.1'
+    },
+    'r2': {
+        'location': 'Samarkand',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.2'
+    },
+    'sw1': {
+        'location': 'Tashkent',
+        'vendor': 'Cisco',
+        'model': '3850',
+        'ios': '3.6.XE',
+        'ip': '10.255.0.101',
+        'vlans': '10,20,30',
+        'routing': True
+    }
+}
