7a0664e7d7abf7be57b54a893ddb471473a553f6
a-ptrv <a.ptrv@yandex.ru>
2023-12-14 14:51:47 +0500

    task1

--- lesson07/nesting/task01.py
+++ lesson07/nesting/task01.py
@@ -23,3 +23,29 @@ devices = {
         'routing': True
     }
 }
+dvc = []
+for i in devices:
+    dvc.append(i)
+delim = '/'
+dvc_str = delim.join(dvc)
+
+device = input(f'Введите название устройства [{dvc_str}]: ')
+
+if device in devices:
+    location = devices.get(device).get('location')
+    print(f'Расположен          {location}')
+    vendor = devices.get(device).get('vendor')
+    print(f'Вендор              {vendor}')
+    model = devices.get(device).get('model')
+    print(f'Модель              {model}')
+    ios = devices.get(device).get('ios')
+    print(f'Система (ios)       {ios}')
+    ip = devices.get(device).get('ip')
+    print(f'IP адрес            {ip}')
+    vlans = devices.get(device).get('vlans')
+    print(f'Выделенные сети     {vlans}')
+    routing = devices.get(device).get('routing')
+    print(f'Маршрутизатор       {routing}')
+else:
+    print('Устройство не существует')
+
